e0d5e4094269c460a174bf32a4ffc1d8370ea27c
Raicuparta <Raicuparta@users.noreply.github.com>
2022-04-30 12:25:38 +0200

    Initial commit

---
+++ StanleyVr/Patches.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace StanleyVr;
+
+[HarmonyPatch]
+public static class Patches
+{
+    private static readonly string[] canvasesToIgnore =
+    {
+        "com.sinai.unityexplorer_Root", // UnityExplorer.
+        "com.sinai.unityexplorer.MouseInspector_Root" // UnityExplorer.
+    };
+    
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(CanvasScaler), "OnEnable")]
+    private static void MoveCanvasesToWorldSpace(CanvasScaler __instance)
+    {
+        Debug.Log($"Found CanvasScaler {__instance.name}");
+    
+        var canvas = __instance.GetComponent<Canvas>();
+
+        if (canvasesToIgnore.Contains(canvas.name)) return;
+        
+        if (canvas.renderMode == RenderMode.ScreenSpaceCamera)
+        {
+            __instance.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
+        }
+        else if (canvas.renderMode == RenderMode.ScreenSpaceOverlay)
+        {
+            canvas.worldCamera = Camera.current;
+            canvas.renderMode = RenderMode.ScreenSpaceCamera;
+            __instance.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(MainCamera), "Start")]
+    private static void FixCameraScale(MainCamera __instance)
+    {
+        __instance.transform.parent.localScale = Vector3.one * 0.5f;
+    }
+}
---
+++ StanleyVr/Plugin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+
+namespace StanleyVr
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            InputTracking.disablePositionalTracking = true;
+            
+            Invoke(nameof(StartGame), 1);
+        }
+
+        private void StartGame()
+        {
+            SceneManager.LoadScene("map1_UD_MASTER");
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.KeypadMinus))
+            {
+                Camera.main.transform.parent.localScale *= 1.1f;
+            }
+            if (Input.GetKeyDown(KeyCode.KeypadPlus))
+            {
+                Camera.main.transform.parent.localScale *= 0.9f;
+            }
+        }
+    }
+}
