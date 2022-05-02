5868247ff37490c6f6400b5c6f352aebdb619ea9
Raicuparta <raicuparta@gmail.com>
2022-05-02 23:47:21 +0200

    Add unity project with UI stuff

---
+++ StanleyVrUnity/Assets/Scripts/VrUi.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrUi : MonoBehaviour
+{
+    [SerializeField]
+    private Camera uiCamera;
+
+    [SerializeField]
+    private RenderTexture renderTexture;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        var canvases = FindObjectsOfType<Canvas>();
+
+        foreach (var canvas in canvases)
+        {
+            canvas.worldCamera = uiCamera;
+            canvas.renderMode = RenderMode.ScreenSpaceCamera;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
