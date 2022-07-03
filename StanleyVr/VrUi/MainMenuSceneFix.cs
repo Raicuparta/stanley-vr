1268dca5ba01821961d34d4123d69c7949d1cf7e
Raicuparta <raicuparta@gmail.com>
2022-07-03 14:39:09 +0200

    fix base main menu

---
+++ StanleyVr/VrUi/MainMenuSceneFix.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace StanleyVr.VrUi;
+
+public class MainMenuSceneFix: MonoBehaviour
+{
+    public static void Create()
+    {
+        new GameObject("VrMainMenuSceneFix").AddComponent<MainMenuSceneFix>();
+    }
+    
+    private void Awake()
+    {
+	    EnableMenuScene();
+        
+		SceneManager.activeSceneChanged += OnSceneChanged;
+    }
+
+	private void OnSceneChanged(Scene arg0, Scene arg1)
+	{
+		EnableMenuScene();
+	}
+
+	private void EnableMenuScene()
+	{
+		var backgroundScenes = GameObject.Find("Background Scenes");
+        if (!backgroundScenes) return;
+
+        var menuBackground = backgroundScenes.transform.Find("MenuBackground-PC");
+        
+        menuBackground.Find("PC Background Camera").gameObject.SetActive(true);
+        menuBackground.Find("Menu_StanleysOffice_DYNAMIC").gameObject.SetActive(true);
+        menuBackground.Find("Menu_StanleysOffice_STATIC").gameObject.SetActive(true);
+	}
+}
