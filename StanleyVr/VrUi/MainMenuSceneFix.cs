25a112531dca8534bebefcf4959e579912382631
Raicuparta <raicuparta@gmail.com>
2022-07-10 14:00:11 +0200

    Fix main menu drawing below scenes. Closes #15

--- StanleyVr/VrUi/MainMenuSceneFix.cs
+++ StanleyVr/VrUi/MainMenuSceneFix.cs
@@ -28,6 +28,15 @@ public class MainMenuSceneFix: MonoBehaviour
 		var backgroundScenes = GameObject.Find("Background Scenes");
         if (!backgroundScenes) return;
 
+        var cameras = backgroundScenes.GetComponentsInChildren<Camera>(true);
+        foreach (var camera in cameras)
+        {
+	        // Depth was 100 for some reason in these cameras,
+	        // so I'm lowering it to make sure these cameras draw below the UI camera.
+	        // The UI camera depth can't be increased due to some weird issue.
+	        camera.depth = 1;
+        }
+
         var menuBackground = backgroundScenes.transform.Find("MenuBackground-PC");
         
         menuBackground.Find("PC Background Camera").gameObject.SetActive(true);
