2ab07ddff156d0dedb1137a7196ada87130512b0
Raicuparta <raicuparta@gmail.com>
2022-07-10 16:11:06 +0200

    Cleanup

--- StanleyVr/Effects/StereoPortalRenderer.cs
+++ StanleyVr/Effects/StereoPortalRenderer.cs
@@ -7,7 +7,6 @@ public class StereoPortalRenderer : MonoBehaviour
 {
 	// TODO: should also try moving the portal depending on which eye is being rendered?
 	// To prevent cases where one eye goes in first.
-	// TODO: also detect when head is actually going through the portal instead of just player body.
 	private void OnPreRender()
 	{
 		var camera = Camera.current;
