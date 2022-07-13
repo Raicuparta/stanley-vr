7c654bdaa1bfe61368352940855d0c3b8674ad27
Raicuparta <raicuparta@gmail.com>
2022-07-13 23:50:18 +0200

    Make scale overlay supa big. Closes #18

--- StanleyVr/VrUi/VrUiManager.cs
+++ StanleyVr/VrUi/VrUiManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace StanleyVr.VrUi;
 
@@ -7,6 +8,7 @@ public class VrUiManager: MonoBehaviour
     public Camera UiCamera { get; private set; }
     public static VrUiManager Instance { get; private set; }
     public const float UiCameraDepth = 4;
+    private GameMaster gameMaster;
     
     public static void Create(GameMaster gameMaster)
     {
@@ -14,6 +16,7 @@ public class VrUiManager: MonoBehaviour
         if (Instance) Destroy(Instance.gameObject);
         Instance = new GameObject("VrUiManager").AddComponent<VrUiManager>();
         Instance.transform.SetParent(gameMaster.transform, false);
+        Instance.gameMaster = gameMaster;
     }
 
     private void Awake()
@@ -25,5 +28,12 @@ public class VrUiManager: MonoBehaviour
         // High depth to make this camera draw on top of the others.
         // Making depth higher than 5 flips the VR camera?? What.
         UiCamera.depth = UiCameraDepth;
+    }
+
+    private void Start()
+    {
+        // Fade ins/outs would show as a scare in the middle of the screen due to how VR UI is done.
+        // Scaling it up a ton is a quick fix to make it cover the whole screen.
+        gameMaster.fadeImage.transform.localScale = Vector3.one * 100;
     }
 }
